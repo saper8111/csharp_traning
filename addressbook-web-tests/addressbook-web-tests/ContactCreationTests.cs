d917e5badd037584d3212ddbf04f192fdbe2f135
saper8111 <34764472+saper8111@users.noreply.github.com>
2018-12-10 21:28:00 +0300

    Созданы вспомогательные методы для создания  контактов, улучшена читаемость кода

--- addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
@@ -4,7 +4,9 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 
 namespace WebAddressbookTests
@@ -42,41 +44,51 @@ namespace WebAddressbookTests
         [Test]
         public void ContactCreationTests()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.LinkText("add new")).Click();
-            driver.FindElement(By.Name("firstname")).Click();
+            OpenHomePage();
+            Login("admin", "secret");
+            InitContactCreation();
+            FillContactForm("789", "433", "456", "87878", "ghhjhhnbm", "ghghghgh", "ghghghf", "fgfgfgfg", "fgfgfgg", "fgghhghgh", "1900", "1111");
+            SubmitContactCreation();
+            ReturnToContactPage();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void ReturnToContactPage()
+        {
+            driver.FindElement(By.LinkText("home page")).Click();
+        }
+
+        private void SubmitContactCreation()
+        {
+            driver.FindElement(By.XPath("(//input[@name='submit'])[2]")).Click();
+        }
+
+        private void FillContactForm(string firstname, string middlename, string lastname, string nickname, string address, string home, string mobile, 
+            string work, string fax, string email, string byear, string ayear)
+        {
             driver.FindElement(By.Name("firstname")).Clear();
             driver.FindElement(By.Name("firstname")).SendKeys("789");
-            driver.FindElement(By.Name("middlename")).Click();
             driver.FindElement(By.Name("middlename")).Clear();
             driver.FindElement(By.Name("middlename")).SendKeys("433");
-            driver.FindElement(By.Name("lastname")).Click();
             driver.FindElement(By.Name("lastname")).Clear();
             driver.FindElement(By.Name("lastname")).SendKeys("456");
-            driver.FindElement(By.Name("nickname")).Click();
             driver.FindElement(By.Name("nickname")).Clear();
             driver.FindElement(By.Name("nickname")).SendKeys("87878");
-            driver.FindElement(By.Name("address")).Click();
             driver.FindElement(By.Name("address")).Clear();
             driver.FindElement(By.Name("address")).SendKeys("ghhjhhnbm");
-            driver.FindElement(By.Name("home")).Click();
             driver.FindElement(By.Name("home")).Clear();
             driver.FindElement(By.Name("home")).SendKeys("ghghghgh");
-            driver.FindElement(By.Name("mobile")).Click();
             driver.FindElement(By.Name("mobile")).Clear();
             driver.FindElement(By.Name("mobile")).SendKeys("ghghghf");
-            driver.FindElement(By.Name("work")).Click();
             driver.FindElement(By.Name("work")).Clear();
             driver.FindElement(By.Name("work")).SendKeys("fgfgfgfg");
-            driver.FindElement(By.Name("fax")).Click();
             driver.FindElement(By.Name("fax")).Clear();
             driver.FindElement(By.Name("fax")).SendKeys("fgfgfgg");
-            driver.FindElement(By.Name("email")).Click();
             driver.FindElement(By.Name("email")).Clear();
             driver.FindElement(By.Name("email")).SendKeys("fgghhghgh");
             driver.FindElement(By.Name("bday")).Click();
@@ -85,22 +97,35 @@ namespace WebAddressbookTests
             driver.FindElement(By.Name("bmonth")).Click();
             new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("January");
             driver.FindElement(By.XPath("//option[@value='January']")).Click();
-            driver.FindElement(By.Name("byear")).Click();
             driver.FindElement(By.Name("byear")).Clear();
             driver.FindElement(By.Name("byear")).SendKeys("1900");
-            driver.FindElement(By.Name("aday")).Click();
             new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText("17");
             driver.FindElement(By.XPath("(//option[@value='17'])[2]")).Click();
-            driver.FindElement(By.Name("amonth")).Click();
             new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText("November");
             driver.FindElement(By.XPath("(//option[@value='November'])[2]")).Click();
-            driver.FindElement(By.Name("ayear")).Click();
             driver.FindElement(By.Name("ayear")).Clear();
             driver.FindElement(By.Name("ayear")).SendKeys("1111");
-            driver.FindElement(By.XPath("(//input[@name='submit'])[2]")).Click();
-            driver.FindElement(By.LinkText("home page")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
         }
+
+        private void InitContactCreation()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void Login(string username, string password)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
