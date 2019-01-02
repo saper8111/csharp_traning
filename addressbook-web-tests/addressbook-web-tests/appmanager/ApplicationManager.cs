143cd0ba15090db38c635a0a25eb3c8801e9a967
saper8111 <34764472+saper8111@users.noreply.github.com>
2019-01-02 16:23:40 +0300

    Revert "Revert "Сделана возможность параллельного запуска тестов.""
    This reverts commit 357eb75b75c54038230c5ceff3a81f67942f1274.

--- addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium.Firefox;
@@ -21,6 +22,8 @@ namespace WebAddressbookTests
         protected GroupHelper groupHelper;
         protected ContactHelper contactHelper;
 
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
         public ApplicationManager()
         {
 
@@ -34,16 +37,7 @@ namespace WebAddressbookTests
         contactHelper = new ContactHelper(this);
         }
 
-
-        public IWebDriver Driver
-        {
-            get
-            {
-                return driver;
-            }
-        }
-
-        public void Stop()
+        ~ApplicationManager()
         {
             try
             {
@@ -52,6 +46,24 @@ namespace WebAddressbookTests
             catch (Exception)
             {
                 // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (!app.IsValueCreated)
+            {
+                app.Value = new ApplicationManager();
+            }
+            return app.Value;
+        }
+
+
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
             }
         }
 
